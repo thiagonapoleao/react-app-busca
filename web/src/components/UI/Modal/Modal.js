693591cfe791ea4969290f8b2ae526d463a6ae00
thiago.napoleao <thc.napoleao@gmail.com>
2021-08-05 00:31:48 -0300

    inserido e listando comentarios

--- web/src/components/UI/Modal/Modal.js
+++ web/src/components/UI/Modal/Modal.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './Modal.css';
 
 const portalRoot = document.getElementById('portal-root');
 
-const UIModal = ({ Children, isOpen, onClickClose }) => {
+const UIModal = ({ children, isOpen, onClickClose }) => {
     if (!isOpen) {
         return null;
     }
 
-    return ReactDom.createPortal(
+    return ReactDOM.createPortal(
         <div className="ui-modal__overlay">
             <div className="ui-modal">
                 <button type="button" className="ui-modal__close-button" onClick={onClickClose}>X</button>                
-                {Children}
+                {children}
             </div>
         </div>,
         portalRoot,
